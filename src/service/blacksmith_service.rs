ecf3685ea00b535dbd21aa79fbddc9a011e45093
BumjinKim <21900104@handong.ac.kr>
2025-11-24 18:20:36 +0900

    feat(service): blacksmith_service 기능 수정 - 판매 가격 조정

--- src/service/blacksmith_service.rs
+++ src/service/blacksmith_service.rs
@@ -176,7 +176,7 @@ impl<'repo_life_time> BlacksmithService<'repo_life_time> {
         let price = if sword.level == 0 {
             ((sword.base_price as f32) * 0.9) as u32
         } else {
-            ((sword.base_price as f32) * (1.0 + (sword.level as f32 * 0.5))) as u32
+            ((sword.base_price as f32) * (1.0 + (sword.level as f32 * 0.2))) as u32
         };
 
         return price;

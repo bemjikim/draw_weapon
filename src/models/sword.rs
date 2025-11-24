92367c6b94fdbe8cf030b1f4fcab435d373661f2
BumjinKim <21900104@handong.ac.kr>
2025-11-24 18:08:21 +0900

    feat(models): sword 파일 구조 수정 - display_name: 강화 횟수에 따른 이름 부여 - display_price: 강화 횟수에 따른 가격 부여

--- src/models/sword.rs
+++ src/models/sword.rs
@@ -1,12 +1,24 @@
 #[derive(Clone)]
 pub struct Sword {
-    pub name: String,
+    pub base_name: String,
+    pub display_name: String,
     pub level: u8, // 0..=7
     pub base_price: u32,
+    pub display_price: u32,
 }
 
 impl Sword {
+    pub fn new(base_name: &str, base_price: u32, display_price: u32) -> Self {
+        Self {
+            base_name: base_name.into(),
+            display_name: base_name.into(),
+            level: 0,
+            base_price,
+            display_price,
+        }
+    }
+
     pub fn display(&self) -> String {
-        format!("[{}, {}강]", self.name, self.level)
+        format!("[{}, {}강]", self.display_name, self.level)
     }
 }
